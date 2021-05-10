1386ae8222a4fc6c956fdd13dcbc24c7de53802f
hankthemason <42900752+hankthemason@users.noreply.github.com>
2021-05-10 11:24:16 -0400

    added projection and path generator; basic map is working

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState  } from 'react'
+import React, { useEffect, useState, useRef  } from 'react'
 import { useFetch } from 'hooks/useDataApi'
 import * as d3 from 'd3'
 
@@ -7,6 +7,37 @@ const url = 'https://raw.githubusercontent.com/dwillis/nyc-maps/master/police_pr
 export const Map = () => {
 
   const [{ data }] = useFetch(url)
+
+  const svgRef = useRef();
+
+  const renderMap = (mapData, path) => {
+    d3.select(svgRef.current)
+      .selectAll('path')
+      .data(mapData.features)
+      .enter()
+      .append('path')
+      .attr('id', d => `precinct-${d.properties['Precinct']}`)
+      .attr('d', path)
+      .attr('stroke', '#000000')
+      .attr('stroke-width', '.2')
+      .attr('fill', 'transparent')
+    
+  }
+  
+  useEffect(() => {
+
+    const height = svgRef.current.clientHeight
+    const width = svgRef.current.clientWidth
+
+    const projection = d3.geoAlbers().fitSize([height, width], data)
+
+	  const pathGenerator = d3.geoPath().projection(projection) 
+    if (data) {
+      renderMap(data, pathGenerator)
+    }
+  }, [data])
  
-  return null
+  return (
+    <svg id='precincts-map' ref={svgRef} width="500" height="500"/>
+  )
 }
