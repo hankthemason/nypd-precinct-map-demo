0d47949198890c35e3c58e0c6442e7fac0412963
hankthemason <42900752+hankthemason@users.noreply.github.com>
2021-05-10 14:50:09 -0400

    added initial tooltip functionality but it's not rendering

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -15,7 +15,37 @@ export const PrecinctsMap = (props) => {
 
   const svgRef = useRef();
 
+  const tooltipMouseOver = (event, d) => {
+    
+    const numAllegations = allegationsByPrecinct.get(d.properties.Precinct)
+
+    const rect = d3.select('#precincts-map').node().getBoundingClientRect()
+		let tooltip = d3.select(".tooltip")
+		
+    //3. tooltip mouseover
+		tooltip
+		  .style("left", (event.pageX - rect.x) + "px")
+  	  .style("top", (event.pageY - rect.y + 20) + "px")
+		  .transition()
+   	  .duration(100)
+   	  .style("opacity", .9)
+    
+    tooltip.html("<strong> Precinct: " + d.properties.Precinct + "</strong>"
+     								+ "<br>" + `allegations: ${numAllegations}`)
+  }
+  
   const renderMap = (mapData, path) => {
+
+    //1. create the tooltip
+    const tooltip = d3.select(svgRef.current)
+			.append("div")
+    	.attr("class", "tooltip")
+    	.style("position", "absolute")
+    	.style("opacity", 0)
+    	.style("pointer-events", "none")
+    	.style("border", "1px solid black")
+    	.style("background", "white")
+    
     d3.select(svgRef.current)
       .selectAll('path')
       .data(mapData.features)
@@ -28,6 +58,13 @@ export const PrecinctsMap = (props) => {
           const numAllegations = allegationsByPrecinct.get(d.properties['Precinct'])
           return scale(numAllegations)
         })
+        //2. mouseon/mouseover
+        .on("mouseover mousemove", tooltipMouseOver)
+        .on("mouseout", function(event, d) {
+          tooltip.transition()
+           .duration(100)
+            .style("opacity", 0);
+        });
   }
   
   useEffect(() => {
